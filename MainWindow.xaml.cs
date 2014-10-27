80a7c1f0ef1666a24588517d8b0dd8c5e788b1d5
ZhuGongpu <zhugongpu@hotmail.com>
2014-10-27 18:40:23 +0800

    添加初始代码

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using SharpDX;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using Device = SharpDX.Direct3D11.Device;
+using MapFlags = SharpDX.Direct3D11.MapFlags;
+
+namespace DXGI_DesktopDuplication
+{
+    /// <summary>
+    ///     MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //todo test code here
+            Capture();
+        }
+
+
+        public void Capture()
+        {
+            // # of graphics card adapter
+            const int numAdapter = 0;
+
+            // # of output device (i.e. monitor)
+            const int numOutput = 0;
+
+            const string outputFileName = "ScreenCapture.bmp";
+
+            // Create DXGI Factory1
+            var factory = new Factory1();
+            var adapter = factory.GetAdapter1(numAdapter);
+
+            // Create device from Adapter
+            var device = new Device(adapter);
+
+            // Get DXGI.Output
+            var output = adapter.GetOutput(numOutput);
+            var output1 = output.QueryInterface<Output1>();
+
+            // Width/Height of desktop to capture
+            int width = output.Description.DesktopBounds.Width;
+            int height = output.Description.DesktopBounds.Height;
+
+            // Create Staging texture CPU-accessible
+            var textureDesc = new Texture2DDescription
+            {
+                CpuAccessFlags = CpuAccessFlags.Read,
+                BindFlags = BindFlags.None,
+                Format = Format.B8G8R8A8_UNorm,
+                Width = width,
+                Height = height,
+                OptionFlags = ResourceOptionFlags.None,
+                MipLevels = 1,
+                ArraySize = 1,
+                SampleDescription = { Count = 1, Quality = 0 },
+                Usage = ResourceUsage.Staging
+            };
+            var screenTexture = new Texture2D(device, textureDesc);
+
+            // Duplicate the output
+            var duplicatedOutput = output1.DuplicateOutput(device);
+
+            bool captureDone = false;
+            for (int i = 0; !captureDone; i++)
+            {
+                try
+                {
+                    SharpDX.DXGI.Resource screenResource;
+                    OutputDuplicateFrameInformation duplicateFrameInformation;
+
+                    // Try to get duplicated frame within given time
+                    duplicatedOutput.AcquireNextFrame(10000, out duplicateFrameInformation, out screenResource);//get new frame
+
+                    //TODO if still holding old frame, destroy it
+
+                    
+                    //TODO QI for IDXGIResource
+                    
+
+                    //get metadata
+                    if (duplicateFrameInformation.TotalMetadataBufferSize > 0)
+                    {
+                        //TODO old buffer size too small
+
+
+
+                        //TODO get move rectangles
+                        int bufferSize = duplicateFrameInformation.TotalMetadataBufferSize;
+                       // duplicatedOutput.GetFrameMoveRects();
+
+
+                    }
+
+                    
+
+
+
+                    if (i > 0)
+                    {
+                        // copy resource into memory that can be accessed by the CPU
+                        using (var screenTexture2D = screenResource.QueryInterface<Texture2D>())
+                            device.ImmediateContext.CopyResource(screenTexture2D, screenTexture);
+
+                        // Get the desktop capture texture
+                        var mapSource = device.ImmediateContext.MapSubresource(screenTexture, 0, MapMode.Read, MapFlags.None);
+
+                        // Create Drawing.Bitmap
+                        var bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+                        var boundsRect = new System.Drawing.Rectangle(0, 0, width, height);
+
+                        // Copy pixels from screen capture Texture to GDI bitmap
+                        var mapDest = bitmap.LockBits(boundsRect, ImageLockMode.WriteOnly, bitmap.PixelFormat);
+                        var sourcePtr = mapSource.DataPointer;
+                        var destPtr = mapDest.Scan0;
+                        for (int y = 0; y < height; y++)
+                        {
+                            // Copy a single line 
+                            Utilities.CopyMemory(destPtr, sourcePtr, width * 4);
+
+                            // Advance pointers
+                            sourcePtr = IntPtr.Add(sourcePtr, mapSource.RowPitch);
+                            destPtr = IntPtr.Add(destPtr, mapDest.Stride);
+                        }
+
+                        // Release source and dest locks
+                        bitmap.UnlockBits(mapDest);
+                        device.ImmediateContext.UnmapSubresource(screenTexture, 0);
+
+                        // Save the output
+                        bitmap.Save(outputFileName);
+
+                        // Capture done
+                        captureDone = true;
+                    }
+
+                    screenResource.Dispose();
+                    duplicatedOutput.ReleaseFrame();
+
+                }
+                catch (SharpDXException e)
+                {
+                    if (e.ResultCode.Code != SharpDX.DXGI.ResultCode.WaitTimeout.Result.Code)
+                    {
+                        throw e;
+                    }
+                }
+            }
+
+            // Display the texture using system associated viewer
+            System.Diagnostics.Process.Start(Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, outputFileName)));
+
+            // TODO: We should cleanp up all allocated COM objects here
+        }
+    }
+}
